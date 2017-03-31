c2e98f0bfd69adb1d14ebf3897877cef49d136a1
Patrick Pierson <patrick.c.pierson@gmail.com>
2017-03-30 22:10:30 -0400

    updated export to print maps

--- export/views.py
+++ export/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 import requests
+import pprint
 
 def list_maps(request):
     url = 'http://spires2.cityoffrederick.com/ArcGIS/rest/services/?f=json&pretty=true'
     maps = requests.get(url)
-    print(maps.json())
-    return HttpResponse(maps)
+    services = []
+    for map in maps.json()['services']:
+        if map['type'] == 'MapServer':
+            services.append('<a href="/export/map/%s">%s</a><br>' % ( map['name'], map['name'] ))
+    return HttpResponse(services)
 
 def test(request):
     return HttpResponse('Ok')
