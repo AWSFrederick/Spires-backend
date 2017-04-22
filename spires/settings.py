1f624ec9d775296ac1d20c5ee475065fac2c7eb7
Patrick Pierson <patrick.c.pierson@gmail.com>
2017-04-22 16:13:15 -0400

    switching to postgres

--- spires/settings.py
+++ spires/settings.py
@@ -74,11 +74,22 @@ WSGI_APPLICATION = 'spires.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mapfrederick',
+        'USER': 'mapfrederick',
+        'PASSWORD': 'mapfrederick',
+        'HOST': 'database.rds.mapfrederick.city',
+        'PORT': '5432',
+     }
 }
 
 
